62271e99c6fd015b922a997e34ca503dd20b4081
Jose <joseangelmoreno1333@gmail.com>
2025-05-24 17:24:57 -0400

    nvm añadidos mas carpetas añadidas

--- src/db.js
+++ src/db.js
@@ -1,12 +1,13 @@
 import pg from 'pg'
+import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
 const {Pool} = pg
 
 const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'bdd_bienes',
-    password:'1234',
-    port:5432
+    user:DB_USER,
+    host:DB_HOST,
+    database:DB_DATABASE,
+    password:DB_PASSWORD,
+    port:DB_PORT
 
 })
 
