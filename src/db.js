3b6070a8c5bbe1ec598dca1078792a419f823ccf
Jose <joseangelmoreno1333@gmail.com>
2025-05-24 20:28:37 -0400

    se implemento squalize , en model se puso la estructura de la tabla departments

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import pg from 'pg'
+import { Sequelize } from 'sequelize';
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_PORT, DB_USER } from './config.js'
 const {Pool} = pg
+
+
 
 const pool = new Pool({
     user:DB_USER,
@@ -11,4 +14,12 @@ const pool = new Pool({
 
 })
 
-export default pool
+
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
+    dialect: 'postgres',
+    logging: false,
+});
+
+export { pool, sequelize };
+
+
