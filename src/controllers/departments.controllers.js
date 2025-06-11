c2e30085ccfd57eeb8689064e67fa5492959dc75
Jose <joseangelmoreno1333@gmail.com>
2025-06-11 14:01:13 -0400

    paseeeeeeeeee

--- src/controllers/departments.controllers.js
+++ src/controllers/departments.controllers.js
@@ -1,83 +1,78 @@
-import { pool } from '../db.js';
-import { getDept,getDeptid ,postDept,putDept,deleteDept} from "../models/department.model.js"
-
-
-//---------------------------------Get---------------------------------------
-export const getDepartments  = async (req,res)=>{
-    try{
-        const rows = await getDept()
-        res.json(rows);
-
-    }catch(error){
-        console.error("Error getting department:", error);
-        res.status(500).send("Error getting department");
-    }
-}
-
-
+import { pool } from "../db.js";
+import {
+  getDept,
+  getDeptid,
+  postDept,
+  putDept,
+  deleteDept,
+} from "../models/departments.model.js";
 
 //---------------------------------Get---------------------------------------
-export const getDepartmentsId = async(req,res)=>{
-    try{
-        const id=req.params
-        const rows = await getDeptid(id)
+export const getDepartments = async (req, res) => {
+  try {
+    const rows = await getDept();
+    res.json(rows);
+  } catch (error) {
+    console.error("Error getting department:", error);
+    res.status(500).send("Error getting department");
+  }
+};
 
-        if (!rows || rows.length == 0){
-            return res.status(404).json({ messaje : "Department not found"});
-        }
-        res.json(rows);
+//---------------------------------Get---------------------------------------
+export const getDepartmentsId = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const rows = await getDeptid(id);
 
-    }catch(error){
-        console.error("Error getting department:", error);
-        res.status(500).send("Error getting department");
+    if (!rows || rows.length == 0) {
+      return res.status(404).json({ messaje: "Department not found" });
     }
-}
-
+    res.json(rows);
+  } catch (error) {
+    console.error("Error getting department:", error);
+    res.status(500).send("Error getting department");
+  }
+};
 
 //-------------------------------Post-----------------------------------------
-export const postDeparments = async(req,res)=>{
-    try{
-        const data=req.body
-        const  rows = await postDept(data)
-        return res.json(rows)
-    }   
-    catch(error){
-        console.error("Error when creating department:", error);
-        res.status(500).send("Error when creating department");
-    } 
-}
+export const postDeparments = async (req, res) => {
+  try {
+    const data = req.body;
+    const rows = await postDept(data);
+    return res.json(rows);
+  } catch (error) {
+    console.error("Error when creating department:", error);
+    res.status(500).send("Error when creating department");
+  }
+};
 
 //--------------------------------Put----------------------------------------
-export const putDeparments = async(req,res)=>{
-    try{
-        const id = req.params.id
-        const data = req.body
-        const rows = await putDept(id,data)
-        res.json(rows);
-    }
-    catch(error){
-        console.error("Error updating department:", error);
-        res.status(500).send("Error updating department");
-    }
-}
-
+export const putDeparments = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = req.body;
+    const rows = await putDept(id, data);
+    res.json(rows);
+  } catch (error) {
+    console.error("Error updating department:", error);
+    res.status(500).send("Error updating department");
+  }
+};
 
 //-------------------------------Delete-----------------------------------------
 
-export const deleteDepartments = async(req,res)=>{
-    try{
-        const id=req.params.id
-        const rows = await deleteDept(id)
+export const deleteDepartments = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const rows = await deleteDept(id);
 
-        if (rows===0){
-            return res.status(404).json({ message : "Department not found"});
-        }
-        else{
-            return res.json({message:"delete department"});
-        }
-    }catch(error){
-        console.error("Error getting department:", error);
-        res.status(500).send("Error getting department");
+    if (rows === 0) {
+      return res.status(404).json({ message: "Department not found" });
+    } else {
+      return res.json({ message: "delete department" });
     }
-}
-    
+  } catch (error) {
+    console.error("Error getting department:", error);
+    res.status(500).send("Error getting department");
+  }
+};
--- src/routes/departments.routes.js
+++ src/routes/departments.routes.js
@@ -1,27 +1,32 @@
 import { Router } from "express";
 
-import {getDepartments,getDepartmentsId,postDeparments,putDeparments,deleteDepartments} from "../controllers/departments.controllers.js"
+import {
+  getDepartments,
+  getDepartmentsId,
+  postDeparments,
+  putDeparments,
+  deleteDepartments,
+} from "../controllers/departments.controllers.js";
 
-const router=Router()
+const router = Router();
 
-//callback(funcion de retorno) la funcion en si 
+//callback(funcion de retorno) la funcion en si
 //request(peticion) req
 //response(respuesta) res
 
 //---------------------------------Get---------------------------------------
-router.get('/departments',getDepartments)
-
+router.get("/departments", getDepartments);
 
 //------------------------------Get------------------------------------------
-router.get('/departments/:id',getDepartmentsId)
+router.get("/departments/:id", getDepartmentsId);
 
 //-------------------------------Post-----------------------------------------
-router.post('/department',postDeparments)
+router.post("/departments", postDeparments);
 
 //--------------------------------Put----------------------------------------
-router.put('/departments/:id',putDeparments)
+router.put("/departments/:id", putDeparments);
 
 //-------------------------------Delete-----------------------------------------
-router.delete('/departments/:id',deleteDepartment)
+router.delete("/departments/:id", deleteDepartments);
 
 export default router;
